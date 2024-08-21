7485f5b0dd8903fd9a4718607398b126bebf198a
Zaime Zaman <141116055+zaime-zaman@users.noreply.github.com>
2024-08-21 21:45:57 +0500

    Add files via upload

---
+++ Leap_year_pro.py
@@ -0,0 +1,6 @@
+print("This is the program of the leap year")
+a=int(input("Enter the year which you want to check it weather it is leap year or not :"))
+if(a%4==0):
+    print("This is the leap year")
+else:
+    print("This is not the leap year")
